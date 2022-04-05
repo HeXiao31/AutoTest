f0f5d12f91570b3606489a8d68420cb332d4e025
hexiao <hexiao_sy@163.com>
2022-04-05 22:42:11 +0800

    接口测试

---
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+
+    @Test
+    public void testCase() {
+        System.out.println("测试用例1");
+    }
+}
